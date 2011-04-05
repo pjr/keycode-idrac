d427eb0c55e7aca6634e67e959ab2fc9ba34d9fb
Philip Reynolds <philip.reynolds@gmail.com>
2011-04-05 21:38:13 +0100

    Fixed formatting.

--- keycode-hack.c
+++ keycode-hack.c
@@ -24,82 +24,82 @@ static FILE *fd = NULL;
 static void
 hack_init(void)
 {
-	void *h;
+  void *h;
 
-	h = dlopen("libX11.so", RTLD_LAZY);
-	if (h == NULL) {
-		fprintf(stderr, "Unable to open libX11\n");
-		_exit(1);
-	}
+  h = dlopen("libX11.so", RTLD_LAZY);
+  if (h == NULL) {
+    fprintf(stderr, "Unable to open libX11\n");
+    _exit(1);
+  }
 
-	real_XNextEvent = dlsym(h, "XNextEvent");
-	if (real_XNextEvent == NULL) {
-		fprintf(stderr, "Unable to find symbol\n");
-		_exit(1);
-	}
+  real_XNextEvent = dlsym(h, "XNextEvent");
+  if (real_XNextEvent == NULL) {
+    fprintf(stderr, "Unable to find symbol\n");
+    _exit(1);
+  }
 
-	real_XKeysymToKeycode = dlsym(h, "XKeysymToKeycode");
-	if (real_XKeysymToKeycode == NULL) {
-		fprintf(stderr, "Unable to find symbol\n");
-		_exit(1);
-	}
+  real_XKeysymToKeycode = dlsym(h, "XKeysymToKeycode");
+  if (real_XKeysymToKeycode == NULL) {
+    fprintf(stderr, "Unable to find symbol\n");
+    _exit(1);
+  }
 
-	sym_XKeycodeToKeysym = dlsym(h, "XKeycodeToKeysym");
-	if (sym_XKeycodeToKeysym == NULL) {
-		fprintf(stderr, "Unable to find symbol\n");
-		_exit(1);
-	}
+  sym_XKeycodeToKeysym = dlsym(h, "XKeycodeToKeysym");
+  if (sym_XKeycodeToKeysym == NULL) {
+    fprintf(stderr, "Unable to find symbol\n");
+    _exit(1);
+  }
 
 #ifdef DEBUG
-	if (fd == NULL) {
-		fd = fopen("/tmp/keycode-log", "a");
-		if (fd == NULL)
-			fprintf(stderr, "Unable to open key-log\n");
-	}
+  if (fd == NULL) {
+    fd = fopen("/tmp/keycode-log", "a");
+    if (fd == NULL)
+      fprintf(stderr, "Unable to open key-log\n");
+  }
 #endif
 
-	hack_initialised = 1;
+  hack_initialised = 1;
 }
 
 int
 XNextEvent(Display *display, XEvent *event)
 {
-	int r;
+  int r;
   int keycode_new;
 
-	if (!hack_initialised)
-		hack_init();
+  if (!hack_initialised)
+    hack_init();
 
-	r = real_XNextEvent(display, event);
+  r = real_XNextEvent(display, event);
 
-	if (event->type == KeyPress || event->type == KeyRelease) {
-		XKeyEvent *keyevent;
-		KeySym keysym;
+  if (event->type == KeyPress || event->type == KeyRelease) {
+    XKeyEvent *keyevent;
+    KeySym keysym;
 
-		keyevent = (XKeyEvent *)event;
+    keyevent = (XKeyEvent *)event;
 #ifdef DEBUG
-		fprintf(fd, "KeyEvent: %d\n", keyevent->keycode);
-		fflush(fd);
+    fprintf(fd, "KeyEvent: %d\n", keyevent->keycode);
+    fflush(fd);
 #endif
 
-		keysym = sym_XKeycodeToKeysym(display, keyevent->keycode, 0);
+    keysym = sym_XKeycodeToKeysym(display, keyevent->keycode, 0);
     keycode_new = keyevent->keycode;
-		switch (keysym) {
-		case XK_Up:
-			keycode_new = 98;
-			break;
-		case XK_Down:
-			keycode_new = 104;
-			break;
-		case XK_Left:
-			keycode_new = 100;
-			break;
-		case XK_Right:
-			keycode_new = 102;
-			break;
-		case XK_Print:
-			keycode_new = 111;
-			break;
+    switch (keysym) {
+    case XK_Up:
+      keycode_new = 98;
+      break;
+    case XK_Down:
+      keycode_new = 104;
+      break;
+    case XK_Left:
+      keycode_new = 100;
+      break;
+    case XK_Right:
+      keycode_new = 102;
+      break;
+    case XK_Print:
+      keycode_new = 111;
+      break;
     case XK_Num_Lock:
       if (event->type == KeyPress) {
         enabled = ( enabled == 1 ? 0 : 1 );
@@ -109,29 +109,29 @@ XNextEvent(Display *display, XEvent *event)
 #endif
       }
       break;
-		}
+    }
 
     if (enabled == 1) 
       keyevent->keycode = keycode_new;
-	}
+  }
 
-	return r;
+  return r;
 }
 
 #ifdef DEBUG
 KeyCode
 XKeysymToKeycode(Display *display, KeySym keysym)
 {
-	KeyCode keycode;
+  KeyCode keycode;
 
-	if (!hack_initialised)
-		hack_init();
+  if (!hack_initialised)
+    hack_init();
 
-	keycode = real_XKeysymToKeycode(display, keysym);
+  keycode = real_XKeysymToKeycode(display, keysym);
 
-	fprintf(fd, "XKeysymToKeycode: %d\n", keycode);
-	fflush(fd);
+  fprintf(fd, "XKeysymToKeycode: %d\n", keycode);
+  fflush(fd);
 
-	return keycode;
+  return keycode;
 }
 #endif
